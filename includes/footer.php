c98e517d445a0fa7a315e6842e41c47b84bc47d1
Swarna Shikhar Das <75322356+SwarnaShikhar@users.noreply.github.com>
2023-02-13 10:11:24 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,62 @@
+        <!-- Footer -->
+    </div>
+        <footer id="myFooter" >
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <h5>Get started</h5>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="registration.php">Sign up</a></li>
+                        <li><a href="#">Downloads</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <h5>About us</h5>
+                    <ul>
+                        <li><a href="#">Company Information</a></li>
+                        <li><a href="#">Contact us</a></li>
+                        <li><a href="#">Reviews</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <h5>Support</h5>
+                    <ul>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Help desk</a></li>
+                        <li><a href="#">Forums</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <h5>Legal</h5>
+                    <ul>
+                        <li><a href="#">Terms of Service</a></li>
+                        <li><a href="#">Terms of Use</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="social-networks">
+            <a href="https://twitter.com/places/0e8d6c6679d45000?lang=zh-Hant" class="twitter"><i class="fa fa-twitter"></i></a>
+            <a href="https://www.facebook.com/sittechno.org/" class="facebook"><i class="fa fa-facebook"></i></a>
+        </div>
+        <div class="footer-copyright">
+            <p>© 2023 Copyright <a href ="https://www.sittechno.org/">Siliguri Institute of Technology </a></p>
+        </div>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <!-- <script src="js/jquery.js"></script> -->
+
+    <!-- Bootstrap Core JavaScript -->
+   <!--  <script src="js/bootstrap.min.js"></script> -->
+
+</body>
+
+</html>
---
+++ includes/widget.php
@@ -0,0 +1,5 @@
+<div class="well">
+    <h4>Contact Us</h4>
+    
+    <a href="https://www.sittechno.org/">Find us Here!</a>
+</div>
